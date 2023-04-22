ca2e1f5f53985741e0160508ed94b4cbed189fa2
hiroki <you@example.com>
2023-04-22 03:41:16 +0000

    user_index修正

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -4,7 +4,7 @@ class Public::SearchesController < ApplicationController
       def search
           @range = params[:range]
           #検索結果がuserの場合
-       if @range == "User"
+       if @range == "会員"
           @users = User.looks(params[:search], params[:word])
           #検索結果がtweetの場合
        else
