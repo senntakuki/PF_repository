6da47609ba928cc44cd410e776e623c50ec9a855
hiroki <you@example.com>
2023-04-25 10:44:43 +0000

    seed追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,12 +9,25 @@ Admin.create!(
     email: 'legends@gmail.com',
     password: 'legends'
   )
-  
+
 users = User.create!(
   [
    {email: 'tabaka@example.com', name: '田中', password: 'tanaka', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user1.jpg"), filename:"sample-user1.jpg")},
    {email: 'saitou@example.com', name: '斉藤', password: 'saitou', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user2.jpg"), filename:"sample-user2.jpg")},
    {email: 'mizuno@example.com', name: '水野', password: 'mizuno', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user3.jpg"), filename:"sample-user3.jpg")},
    {email: 'suzuki@example.com', name: '鈴木', password: 'suzuki', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user4.jpg"), filename:"sample-user4.jpg")},
+   {email: 'michael@example.com', name: 'マイケル', password: 'michael', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user5.jpg"), filename:"sample-user4.jpg")},
   ]
 )
+
+ tweet = Tweet.create!(
+   [
+    {title: '鷲ヶ岳',   body: 'レストランおすすめです。',           user_id: users[0].id},
+    {title: 'ニセコ',   body: 'コースが豊富でした。',               user_id: users[0].id},
+    {title: '高鷲',     body: '上級者向けでした。',                 user_id: users[1].id},
+    {title: '旭岳',     body: 'ファミリー向けでした。',             user_id: users[1].id},
+    {title: '富良野',   body: '初心者でも滑りやすかったです。',     user_id: users[2].id},
+    {title: '石狩',     body: '締めの温泉最高でした。',             user_id: users[3].id},
+    {title: '白馬五竜', body: '山頂からの景色は最高でした。',       user_id: users[4].id},
+       ]
+     )
