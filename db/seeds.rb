39488d1c72d6086f89c48abb0aa7c2051ead720d
hiroki <you@example.com>
2023-04-25 09:24:50 +0000

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,12 @@ Admin.create!(
     email: 'legends@gmail.com',
     password: 'legends'
   )
+  
+users = User.create!(
+  [
+   {email: 'tabaka@example.com', name: '田中', password: 'tanaka', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user1.jpg"), filename:"sample-user1.jpg")},
+   {email: 'saitou@example.com', name: '斉藤', password: 'saitou', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user2.jpg"), filename:"sample-user2.jpg")},
+   {email: 'mizuno@example.com', name: '水野', password: 'mizuno', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user3.jpg"), filename:"sample-user3.jpg")},
+   {email: 'suzuki@example.com', name: '鈴木', password: 'suzuki', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user4.jpg"), filename:"sample-user4.jpg")},
+  ]
+)
